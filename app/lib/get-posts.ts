ce161ae8118aa1e777d1a76311563deca171d68a
rivea0 <58330360+rivea0@users.noreply.github.com>
2024-01-18 22:34:39 +0300

    Add selected posts to homepage

--- app/lib/get-posts.ts
+++ app/lib/get-posts.ts
@@ -26,6 +26,30 @@ export default function getPosts() {
   return filtered
 }
 
+export const selectedPosts = [
+  {
+    slug: 'the-curious-case-of-recursive-and-iterative-processes',
+    title: 'The curious case of recursive and iterative processes',
+    date: '2023-12-23',
+  },
+  {
+    slug: 'a-reminder-to-use-single-quotes-when-writing-commit-messages',
+    title: 'A reminder to use single quotes when writing commit messages',
+    date: '2023-07-13',
+  },
+  {
+    slug: 'a-quick-behind-the-scenes-of-greedy-and-lazy-matching',
+    title: 'A Quick Behind the Scenes of Greedy and Lazy Matching',
+    date: '2022-03-31',
+  },
+  {
+    slug: 'custom-endswith-and-startswith-functions',
+    title: 'Custom endsWith and startsWith Functions',
+    date: '2022-02-22',
+  },
+];
+
+
 // export function getPost(slug: string) {
 //   const posts = getPosts()
 //   return posts.find((post) => post.slug === slug)
