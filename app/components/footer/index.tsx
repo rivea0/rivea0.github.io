55e31e9ba6fa5090b92bf11d5ca2e8257b09a27f
rivea0 <58330360+rivea0@users.noreply.github.com>
2024-02-03 20:56:46 +0300

    Change footer text

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,14 +1,15 @@
-import styles from './footer.module.css'
+import Link from '@components/link';
+import styles from './footer.module.css';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <p>
-        {' '}
-        {new Date().getFullYear()},
-        {' '}
-        Eda Eren.
+        &copy; {new Date().getFullYear()} &middot;{' '}
+        <Link href="https://github.com/rivea0/rivea0.github.io">
+          View source
+        </Link>
       </p>
     </footer>
-  )
+  );
 }
