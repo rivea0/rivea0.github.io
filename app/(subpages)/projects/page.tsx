6f2d17354e8dba3dc81d339c1968e26c38d88906
rivea0 <58330360+rivea0@users.noreply.github.com>
2025-01-15 06:53:56 +0300

    Add leetcode meditations series under writing

--- app/(subpages)/projects/page.tsx
+++ app/(subpages)/projects/page.tsx
@@ -42,15 +42,24 @@ export default function Page() {
         demoHref="https://rivea0.github.io/bite-sized-math-for-cs"
         codeHref="https://github.com/rivea0/bite-sized-math-for-cs"
         isWriting={true}
+        demoName='Website'
       />
       <Project 
         title={<><em>Solving the Problem Sets of CS50's Introduction to Programming with Python — One at a Time</em> Series</>} 
         description={<>A ten-part series of blog posts that were published each week as the new problem sets arrived in <Link href="https://cs50.harvard.edu/python/2022">CS50's Introduction to Programming with Python</Link> to guide learners taking the course.</>} 
-        demoHref="https://dev.to/rivea0/solving-the-problem-sets-of-cs50s-introduction-to-programming-with-python-one-at-a-time-problem-set-0-27d5" 
+        thirdPartyHref="https://dev.to/rivea0/solving-the-problem-sets-of-cs50s-introduction-to-programming-with-python-one-at-a-time-problem-set-0-27d5"
         isWriting={true}
         more={<>I took the original CS50 the year before, and had been diving a bit deep into Python, so when this course was first introduced, I took the challenge to take it, do the problem sets, and write about it each week on the day they arrived. It indeed proved to <Link href="https://web.archive.org/web/20231114183738/https://dev.to/rivea0/solving-the-problem-sets-of-cs50s-introduction-to-programming-with-python-one-at-a-time-problem-set-5-k44#comment-1od25">be</Link> <Link href="https://web.archive.org/web/20231114184027/https://dev.to/rivea0/solving-the-problem-sets-of-cs50s-introduction-to-programming-with-python-one-at-a-time-problem-set-8-5lf#comment-20o22">helpful</Link> for some folks.</>}
-        isThirdParty={true}
         thirdPartyName='dev.to'
+      />
+      <Project
+        title={<><em>LeetCode Meditations</em></>}
+        description={<>A series of 79 blog posts spanning the year of 2024, tackling LeetCode problems (including introductory chapters with hand-made GIFs).</>}
+        demoHref="https://rivea0.github.io/leetcode-meditations"
+        demoName='Series'
+        isWriting={true}
+        thirdPartyName='dev.to'
+        thirdPartyHref='https://dev.to/rivea0/series/26418'
       />
       </ul>
     </Suspense>
