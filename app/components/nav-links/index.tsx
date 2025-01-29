f8bda66a96ae05afb5825eaa864fd0378191a112
rivea0 <58330360+rivea0@users.noreply.github.com>
2025-01-29 03:35:52 +0300

    Style active nav links

--- app/components/nav-links/index.tsx
+++ app/components/nav-links/index.tsx
@@ -1,19 +1,25 @@
+'use client'
+
 import Link from 'next/link'
 import styles from './nav-links.module.css'
+import { usePathname } from 'next/navigation'
+import clsx from 'clsx'
 
 export default function NavLinks() {
+  const pathname = usePathname()
+
   return (
     <div className={styles.nav}>
-      <Link href='/'>
+      <Link href='/' className={clsx(pathname === '/' && styles.activeLink)}>
         Home
       </Link>
-      <Link href='/blog'>
+      <Link href='/blog' className={clsx(pathname === '/blog' && styles.activeLink)}>
         Blog
       </Link>
-      <Link href='/projects'>
+      <Link href='/projects' className={clsx(pathname === '/projects' && styles.activeLink)}>
         Projects, etc.
       </Link>
-      <Link href='/about'>
+      <Link href='/about' className={clsx(pathname === '/about' && styles.activeLink)}>
         About
       </Link>
     </div>
