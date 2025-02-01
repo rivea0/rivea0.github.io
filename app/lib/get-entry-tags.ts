eb02d54cbf1b0c2d80d70e77ef73753c8928aa66
rivea0 <58330360+rivea0@users.noreply.github.com>
2025-02-01 18:50:03 +0300

    Add types for `get-entry-tags`

--- app/lib/get-entry-tags.ts
+++ app/lib/get-entry-tags.ts
@@ -1,7 +1,7 @@
 import { Entry } from './types';
 
-export function getEntryTags(entries: Entry[]) {
-  const allTagsSet = new Set();
+export function getEntryTags(entries: Entry[]): string[] {
+  const allTagsSet = new Set<string>();
   entries.map((entry) => entry.tags.forEach((tag) => allTagsSet.add(tag)));
 
   return Array.from(allTagsSet).sort();
