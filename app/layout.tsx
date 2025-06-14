c97243bf49494787716c8b6cf9085252aec7a1bd
rivea0 <58330360+rivea0@users.noreply.github.com>
2025-06-14 21:11:28 +0300

    Minor font updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${inter.className}`} suppressHydrationWarning>
+    <html lang="en" className={`${inter.variable}`} suppressHydrationWarning>
       <body>
         <ThemeProvider>
           <GCScript
