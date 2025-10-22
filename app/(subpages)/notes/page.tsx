7abad165591336e6fe1788998d7c2da29f8c9530
rivea0 <58330360+rivea0@users.noreply.github.com>
2025-10-23 01:16:02 +0300

    Add note count display, minor style fixes

--- app/(subpages)/notes/page.tsx
+++ app/(subpages)/notes/page.tsx
@@ -1,13 +1,23 @@
 import { Suspense } from 'react';
 import LoaderIcon from '@components/loader-icon';
 import Link from '@components/link';
-import { capitalizeFirstLetter, getNoteCategories } from '@lib/get-entries';
+import { capitalizeFirstLetter, getNoteCategories, getNotesInCategory } from '@lib/get-entries';
 import styles from './notes.module.css';
 
 export default async function Notes() {
   const categories = getNoteCategories();
+
+  let noteCount = 0;
+  categories.forEach((category) => {
+    let notes = getNotesInCategory(category);
+    notes.forEach((note) => {
+      noteCount += 1;
+    });
+  });
+
   return (
     <Suspense fallback={<LoaderIcon />}>
+      <p><em>Total of {noteCount} notes</em></p>
       {categories
         .sort((a: string, b: string) => b.charCodeAt(0) - a.charCodeAt(0))
         .map((category) => {
